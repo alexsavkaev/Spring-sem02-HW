105a6206179d90623df4e0e64aa090bb6e7e93f9
alexsavkaev <lesh1y174@gmail.com>
2024-04-08 22:55:50 +0300

    init commit + HW

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+
+
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
+
+    public void updateUser(User user){
+        userRepository.updateUser(user);
+    }
+}
